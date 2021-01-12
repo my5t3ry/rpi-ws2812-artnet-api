4183c78436ef07bbca0bd56abdbaad02a1971e8e
my5t3ry <sash@mischkonsum.org>
2021-01-12 23:46:04 +0100

    init

---
+++ src/test/java/de/my5t3ry/ledcontroller/LedControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.my5t3ry.ledcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedControllerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

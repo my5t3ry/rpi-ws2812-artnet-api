653b0fedfb6a194d35f0720371c4366f292ed81e
my5t3ry <sash@mischkonsum.org>
2021-01-13 07:53:26 +0100

    init

---
+++ led-controller-web/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'led-controller-web';
+  api: string = "http://192.168.0.10/led-api/led"
+
+  constructor(private http: HttpClient) {
+  }
+
+  togglePower() {
+    this.http.post(this.api, null).toPromise().then(value => {
+      console.log(value);
+    })
+  }
+}
